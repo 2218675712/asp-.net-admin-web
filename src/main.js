752d5b1428cc5f0c0112bd84efe68324e6f24128
2218675712 <2218675712@qq.com>
2021-03-31 08:41:36 +0800

    Vue项目初始化

--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
 import Vue from 'vue'
+import Axios from 'axios'
+import VueAxios from 'vue-axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import VueLazyload from 'vue-lazyload'
+import VueCookie from 'vue-cookie'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, Axios)
+Vue.use(ElementUI)
+// 根据前端的跨域方式做调整
+Axios.defaults.baseURL = '/api'
+// 根据环境变量获取不同的请求地址
+// Axios.defaults.baseURL=env.baseURL
+// 设置请求超时
+Axios.defaults.timeout = 5000
 
+Vue.use(VueCookie)
+Vue.use(VueLazyload, {
+  loading: '/imgs/loading-svg/loading-bars.svg'
+})
 new Vue({
   router,
   store,
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,23 @@
 module.exports = {
+  devServer: {
+    host: 'localhost',
+    port: 8081,
+    proxy: {
+      '/api': {
+        target: 'http://mall-pre.springboot.cn',
+        changeOrigin: true,
+        pathRewrite: {
+          '/api': ''
+        }
+
+      }
+    }
+  },
+  // 不打包map文件
+  productionSourceMap: false,
+  // 删除预加载，真正实现按需加载
+  chainWebpack: (config) => {
+    config.plugins.delete('prefetch')
+  },
   lintOnSave: false
 }
