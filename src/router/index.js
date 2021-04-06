2c2b805a3d967e0726883353fe5765b8ca87c7bc
2218675712 <2218675712@qq.com>
2021-04-06 11:40:58 +0800

    路由空路由拦截

--- src/router/index.js
+++ src/router/index.js
@@ -39,4 +39,11 @@ router.beforeEach((to, from, next) => {
   }
   next()
 }) */
+router.beforeEach((to, from, next) => {
+  if (to.matched.length === 0) { // 如果未匹配到路由
+    from.path ? next({ path: from.path }) : next('/login')// 如果上级也未匹配到路由则跳转主页面，如果上级能匹配到则转上级路由
+  } else {
+    next() // 如果匹配到正确跳转
+  }
+})
 export default router
